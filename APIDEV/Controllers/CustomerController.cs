0ef2f17e1cbb9df15d2d3f52005568e2d31aa581
Markkimaathi <markangelkimathi@gmail.com>
2024-08-27 12:09:16 +0300

    Implement CRUD functions

--- APIDEV/Controllers/CustomerController.cs
+++ APIDEV/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using APIDEV.Service;
+﻿using APIDEV.Modal;
+using APIDEV.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
@@ -15,16 +16,47 @@ namespace APIDEV.Controllers
         {
             this.service = service;
         }
-        [HttpGet("GetAll")]
 
+        [HttpGet("GetAll")]
         public async Task<IActionResult> GetAll()
         {
-                var data =await this.service.Getall();
-                if (data == null)
-                {
-                    return NotFound();
-                }
-                return Ok(data);
+            var data = await this.service.Getall();
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+        [HttpGet("Getbycode")]
+        public async Task<IActionResult> Getbycode(string code)
+        {
+            var data = await this.service.Getbycode(code);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(Customermodal _data)
+        {
+            var data = await this.service.Create(_data);
+            return Ok(data);
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(Customermodal _data, string code)
+        {
+            var data = await this.service.Update(_data,code);
+            return Ok(data);
+        }
+        [HttpDelete("Remove")]
+        public async Task<IActionResult> Remove(Customermodal _data, string code)
+        {
+            var data = await this.service.Remove(code);
+            return Ok(data);
         }
     }
 }
