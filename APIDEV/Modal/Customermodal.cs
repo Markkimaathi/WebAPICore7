f61256710e472468de9d77c84af59efa4795e7ae
Markkimaathi <markangelkimathi@gmail.com>
2024-08-29 11:17:50 +0300

    Excel Implementation

--- APIDEV/Modal/Customermodal.cs
+++ APIDEV/Modal/Customermodal.cs
@@ -1,22 +1,38 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace APIDEV.Modal
 {
+    [Keyless]
     public class Customermodal
     {
-        [Key]
-        [Column("ID")]
-        public int Id { get; set; }
-
+        [StringLength(50)]
+        [Unicode(false)]
         public string Code { get; set; } = null!;
 
-        public string? Name { get; set; }
+        public int Id { get; set; }
 
-        public string? Category { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Email { get; set; }
+
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Phone { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Creditlimit { get; set; }
 
         public int IsActive { get; set; }
 
         public string? Statusname { get; set; }
+
+        public string? Category { get; set; }
     }
 }
